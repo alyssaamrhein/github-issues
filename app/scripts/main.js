dbe8876f98dd4311c3ec7b625d3697f9e33d5582
Alyssa <alyssa@thepalesnail.com>
2014-10-15 14:42:15 -0400

    made changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,43 +1,20 @@
+var issueTemplate = _.template( $('#issueTemplate').html() );
+var issues = $('#issues');
 var apiurl = 'https://api.github.com/repos/alyssaamrhein/weekend-js/issues';
 
 
 var ajaxCall = function () {
   $.ajax({url: apiurl}).done(function(data) {
-    $('.newNew').text(JSON.stringify(data));
-      setIssueInfo(data);
-  }).fail(function (data) {
-
-    $('.error').removeClass('fade').text(data.responseJSON.message);
-
-    setTimeout(function () {
-      $('.error').addClass('fade');
-    }, 1000);
-
-  });
-};
-
-var setIssueInfo = function (data) {
-  _.each(data, function (issue) {
-    var issueTemplateString = $('#theTemplate').html();
-    var issueTemplate = _.template(issueTemplateString);
-    var finishedIssueTemplate = issueTemplate(issue);
-    $('#target').append(finishedIssueTemplate);
-  });
-};
-
-setInterval(function() {
-    $.ajax({
-      url: apiurl
-    }).done(function (data) {
-      $(data.length).text(data.length);
+    var templates = _.map(data, function (issue) {
+      return issueTemplate(issue);
     });
-    // ^^^ Call the api url and update the count to the data.length
-  }, 1000);
-  // ^^^ Using setInterval to repeat every second
+    issues.html(templates);
+  });
+};
 
+setInterval(ajaxCall, 1000);
 
 ajaxCall();
-setIssueInfo();
-setInterval();
+
 
 console.log('The Iron Yard Rocks');
